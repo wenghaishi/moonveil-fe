9e47da2815730662516487ae94e0a6fb8ea4f3b7
weng haishi <wenghaishi1@gmail.com>
2025-10-17 12:24:51 +0800

    initialise fe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,49 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="relative flex flex-col items-center justify-center w-full h-screen font-sans overflow-hidden">
+      <Image
+        src="/bg.png"
+        alt="Background"
+        fill
+        priority
+        className="object-cover object-top dark:invert"
+      />
+
+      <div className="relative h-[30vh] w-full z-10">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/wolfmoon.png"
+          alt="Wolf howling at the moon"
+          fill
+          className="object-contain object-center"
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Your page content goes here */}
+      <div className="relative z-10 flex items-center justify-center h-full w-full flex-col mt-[-350px]">
+        <h1 className="text-5xl md:text-9xl font-bold title-font title-font-color stroke-text font-bold">Moonveil</h1>
+        <div className={'w-1/2 bg-yellow h-1/2 rounded-4xl mt-10 flex flex-col items-center justify-center gap-6 p-4'}>
+          <div className={'title-font text-blue-950 font-bold text-6xl'}>Welcome!</div>
+          <div className={'title-font text-blue-950 text-xl px-16'}>Moonveil is a dark fantasy village shrouded in mist and secrets.
+            When the moon rises, trust fades — and survival becomes a game of lies, deduction, and betrayal.
+            Each night, the villagers sleep uneasily as hidden werewolves stalk among them.</div>
+          <button className={'w-52 h-16 bg-[#D1831C] rounded-xl text-white text-xl font-bold'}>
+            Create account
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      {/*<div className="h-[500px] w-full bottom-0 fixed z-10">*/}
+      {/*  <Image*/}
+      {/*    src="/houses.png"*/}
+      {/*    alt="Wolf howling at the moon"*/}
+      {/*    fill*/}
+      {/*    className="object-cover object-top"*/}
+      {/*    priority*/}
+      {/*  />*/}
+      {/*</div>*/}
+
     </div>
   );
 }
